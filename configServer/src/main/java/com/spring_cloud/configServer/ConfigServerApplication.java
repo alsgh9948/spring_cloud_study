f025131139a1bcd603e78b6a20ea67f313e31f2d
minho.seo <alsgh9948@naver.com>
2022-01-08 17:06:58 +0900

    server config add

--- configServer/src/main/java/com/spring_cloud/configServer/ConfigServerApplication.java
+++ configServer/src/main/java/com/spring_cloud/configServer/ConfigServerApplication.java
@@ -2,8 +2,11 @@ package com.spring_cloud.configServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+@EnableConfigServer
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
